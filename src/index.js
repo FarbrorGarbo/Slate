3fd6397872723fc4ee9d1eeb522f4a87a278ed2f
FarbrorGarbo <ask.jaho@gmail.com>
2019-06-27 09:50:59 +0200

    Slate-lists table bug

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function App() {
       <SlateEditor
         readOnly={false}
         document={
-          "<html><body><p>Select text in the table text and hit the 'Make list' button. The whole table becomes part of the new list instead of selected text inside the table cell</p><table><thead><td>Collumn 1</td><td>Collumn 2</td></thead><tr><td>Some text</td><td>Some text</td></tr></table></body></html>"
+          "<html><body><h3>Slate-lists table bug</h3><p>Select text in the table text and hit the 'Make list' button. The whole table becomes part of the new list instead of selected text inside the table cell</p><table><thead><td>Collumn 1</td><td>Collumn 2</td></thead><tr><td>Some text</td><td>Some text</td></tr></table></body></html>"
         }
         pageLoaded={() => {
           console.log("Page loaded");
